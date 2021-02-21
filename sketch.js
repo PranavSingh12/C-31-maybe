a42a0d3df13d54d7c1a58c5521ff1808de5b4da9
PranavSingh12 <75402436+PranavSingh12@users.noreply.github.com>
2021-02-20 23:50:54 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+var man_image;
+var ground;
+var ground_options;
+var maxDrops = 100;
+var drops = [];
+var thunder1, thunder2, thunder3;
+var umbrella;
+var thunder, thunderCreatedFrame = 0;
+
+
+function setup(){
+  var canvas = createCanvas(300,500);
+
+  engine = Engine.create();
+  world  = engine.world;
+
+  
+  umbrella = new Umbrella();
+
+  if(frameCount%100===0){
+    for(var i=0; i<maxDrops;i++){
+      drops.push(new Drop(random(0,400),random(0,400),3,10));
+     }
+    }
+
+}
+
+function draw(){
+  background(0);
+  Engine.update(engine);
+
+  var rand = Math.round(random(1,2));
+  if(frameCount%80===0){
+    thunderCreatedFrame = frameCount;
+   thunder = createSprite(random(10,370),random(10,30),10,10);
+
+   }
+   console.log(thunderCreatedFrame);
+  }
+  
+  if(thunderCreatedFrame + 20 === frameCount && thunder){
+    thunder.destroy();
+ }
+
+  umbrella.display();
+  
+  for(var i=0; i<maxDrops;i++){
+    drops[i].display();
+    drops[i].update();
+  }
+  camera.position.x = mouseX;
+  
+  drawSprites();
+
+
